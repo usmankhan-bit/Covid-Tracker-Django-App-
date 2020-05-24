0b871b787e388147f4116e6c2ae35fcc38ddd4ef
usmankhan-bit <usmankhannn18@gmail.com>
2020-05-24 16:35:16 +0530

    Details.html added

--- covidcases/urls.py
+++ covidcases/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'covidcases'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('details/<int:country_id>', views.details, name='details')
 ]
--- covidcases/views.py
+++ covidcases/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Country,Patients
 
 # Create your views here.
@@ -11,3 +11,13 @@ def home(request):
     }
 
     return render(request, 'covidcases/home.html', context)
+
+def details(request, country_id):
+    country = get_object_or_404(Country, pk=country_id)
+    all_patients = country.patients_set.all()
+    context = {
+        'all_patients': all_patients
+    }
+
+    return render(request, 'covidcases/details.html', context)
+
